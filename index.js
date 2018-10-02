70bdcff00ccb268a0aca85a63808c1bc9cecc63d
Jelmer de Maat <jelmer@netvlies.nl>
2018-10-02 17:49:36 +0200

    Init repository

---
+++ index.js
@@ -0,0 +1,33 @@
+export function reportValidity (form) {
+    if (!form) {
+        throw Error('Target form element missing for function `reportValidity`.')
+    }
+
+    if (HTMLFormElement.prototype.reportValidity) {
+        return form.reportValidity();
+    } else {
+        if (form.checkValidity()) {
+            return true;
+        }
+
+        if (!form.reportValidityFakeSubmit) {
+            form.reportValidityFakeSubmit = document.createElement('button');
+            form.reportValidityFakeSubmit.setAttribute('type', 'submit');
+            form.reportValidityFakeSubmit.setAttribute('hidden', 'hidden');
+            form.reportValidityFakeSubmit.setAttribute('style', 'display:none');
+            form.reportValidityFakeSubmit.setAttribute('class', 'reportValidityFakeSubmit');
+
+            form.reportValidityFakeSubmit.addEventListener('click', function (evt) {
+                if (form.checkValidity()) {
+                    evt.preventDefault();
+                }
+            });
+
+            form.appendChild(form.reportValidityFakeSubmit);
+        }
+
+        form.reportValidityFakeSubmit.click();
+
+        return false;
+    }
+}
